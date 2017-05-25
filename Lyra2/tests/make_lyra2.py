f1d87a212c4b8be67d2dc5783763ae45f2bccd78
Alexander Lisianoi <all3fox@gmail.com>
2017-05-25 11:46:24 +0200

    Make make_lyra2.py smarter, remove make_lyra2_binaries.py

--- Lyra2/tests/make_lyra2.py
+++ Lyra2/tests/make_lyra2.py
@@ -1,23 +1,18 @@
 #!/usr/bin/env python3
 
 import os
+import sys
+import yaml
 import subprocess
+import itertools
 
 from pathlib import Path
 
-class CompilerFlags:
 
-    def __init__(cflags=None):
-        self.cflags = {} if cflags is None else cflags
+def to_list(x):
+    return x if isinstance(x, list) else [x]
 
-    def __str__(self):
-        pass
-
-def make_lyra2(
-        makefile=None, option=None, bindir=None, binname=None,
-        nCols=256, nThreads=1, nRoundsSponge=1, bSponge=1, sponge=1, bench=0,
-        CFLAGS=None
-):
+def make_lyra2(params):
     """
     Build Lyra2 using the existing makefile
 
@@ -32,108 +27,154 @@ def make_lyra2(
     3. In the implementation reference .pdf
     """
 
-    if makefile is None:
-        # Assume we are in Lyra2/tests and makefile is in Lyra2/src
-        makefile = Path(__file__).resolve().parent.parent.joinpath(
-            "src", "makefile"
-        )
+    try:
+        build_path = Path(params['build_path']).resolve()
+    except KeyError:
+        sys.exit('Please specify build_path')
 
-    if option is None:
-        option = "generic-x86-64"
+    try:
+        makefile_path = Path(params['makefile_path']).resolve()
+    except KeyError:
+        sys.exit('Please specify makefile_path')
 
-    if bindir is None:
-        bindir = makefile.parent.parent.joinpath("bin")
+    try:
+        matrix = params['matrix']
+    except KeyError:
+        sys.exit('Please specify build matrix')
 
-    if binname is None:
-        name = "lyra2-" + option
-        name += "-cols-" + str(nCols)
-        name += "-threads-" + str(nThreads)
-        name += "-nrounds-" + str(nRoundsSponge)
-        name += "-nblocks-" + str(bSponge)
-        name += "-sponge-" + str(sponge)
+    try:
+        option = to_list(matrix['option'])
+    except KeyError:
+        sys.exit('Please specify option')
 
-        binname = bindir.joinpath(name)
+    try:
+        threads = to_list(matrix['threads'])
+    except KeyError:
+        sys.exit('Please specify threads')
 
-    parameters="""parameters=\
-    -DN_COLS={} -DnPARALLEL={} -DRHO={} -DBLOCK_LEN_INT64={} -DSPONGE={} -DBENCH={}\
-    """.format(nCols, nThreads, nRoundsSponge, bSponge, sponge, bench)
+    try:
+        columns = to_list(matrix['columns'])
+    except KeyError:
+        sys.exit('Please specify columns')
 
-    CFLAGS="""\
-    -std=c99 -g -Wall -pedantic -O3 -msse2 \
-    -ftree-vectorizer-verbose=1 -fopenmp \
-    -funroll-loops -march=native -Ofast \
-    -mprefer-avx128 -flto \
-    """
+    try:
+        sponge = to_list(matrix['sponge'])
+    except KeyError:
+        sys.exit('Please specify sponge')
 
-    process = subprocess.run([
-        "make", option,
-        "BINDIR={}".format(bindir),
-        "BIN={}".format(binname),
-        parameters,
-        "CFLAGS={}".format(CFLAGS),
-        "--makefile", makefile,
-        "--directory", makefile.parent,
-    ])
+    try:
+        rounds = to_list(matrix['rounds'])
+    except KeyError:
+        sys.exit('Please specify rounds')
+
+    try:
+        blocks = to_list(matrix['blocks'])
+    except KeyError:
+        sys.exit('Please specify blocks')
+
+    try:
+        bench = to_list(matrix['bench'])
+    except KeyError:
+        sys.exit('Please specify bench')
+
+    try:
+        CFLAGS = ' '.join(to_list(params['CFLAGS']))
+    except KeyError:
+        CFLAGS = ''
+
+    for option, threads, columns, sponge, rounds, blocks, bench in itertools.product(
+            option, threads, columns, sponge, rounds, blocks, bench
+    ):
+
+        name = 'lyra2-' + option
+        name += '-threads-' + str(threads)
+        name += '-columns-' + str(columns)
+        name += '-sponge-' + str(sponge)
+        name += '-rounds-' + str(rounds)
+        name += '-blocks-' + str(blocks)
+
+        parameters = 'parameters='
+        parameters += ' -DnPARALLEL=' + str(threads)
+        parameters += ' -DN_COLS=' + str(columns)
+        parameters += ' -DSPONSE=' + str(sponge)
+        parameters += ' -DRHO=' + str(rounds)
+        parameters += ' -DBLOCK_LEN_INT64=' + str(blocks)
+        parameters += ' -DBENCH=' + str(bench)
+
+        process = subprocess.run([
+            'make', option,
+            'BINDIR=' + str(build_path),
+            'BIN=' + name,
+            parameters,
+            'CFLAGS=' + CFLAGS,
+            '--makefile', str(makefile_path),
+            '--directory', str(makefile_path.parent),
+        ])
 
 
-if __name__ == "__main__":
-    from argparse import ArgumentParser
+if __name__ == '__main__':
+    import yaml
 
-    parser = ArgumentParser(description="Friendly compilation frontend")
+    with open('lyra2.yml', 'r') as config:
+        make_lyra2(yaml.load(config))
 
-    parser.add_argument(
-        "option", default="generic-x86-64", choices=[
-            "generic-x86-64", "linux-x86-64-sse",
-            "cygwin-x86-64", "cygwin-x86-64-sse",
-            "linux-x86-64-cuda", "linux-x86-64-cuda-attack",
-            "clean"
-        ], help="Compilation target for make"
-    )
+    # from argparse import ArgumentParser
 
-    parser.add_argument(
-        "--mcost", type=int, default=256, choices=[
-            16, 32, 64, 96, 128, 256, 512, 1024, 2048
-        ], help="Number of columns, only use tested values"
-    )
+    # parser = ArgumentParser(description="Friendly compilation frontend")
 
-    parser.add_argument(
-        "--nthreads", type=int, default=1,
-        help="Number of threads to use, must be positive"
-    )
+    # parser.add_argument(
+    #     "option", default="generic-x86-64", choices=[
+    #         "generic-x86-64", "linux-x86-64-sse",
+    #         "cygwin-x86-64", "cygwin-x86-64-sse",
+    #         "linux-x86-64-cuda", "linux-x86-64-cuda-attack",
+    #         "clean"
+    #     ], help="Compilation target for make"
+    # )
 
-    parser.add_argument(
-        "--nrounds", type=int, default=1, choices=list(range(1, 13)),
-        help="Number of rounds performed by reduced sponge function"
-    )
+    # parser.add_argument(
+    #     "--mcost", type=int, default=256, choices=[
+    #         16, 32, 64, 96, 128, 256, 512, 1024, 2048
+    #     ], help="Number of columns, only use tested values"
+    # )
 
-    parser.add_argument(
-        "--nblocks", type=int, default=12, choices=[8, 10, 12],
-        help="Number of sponge blocks, bitrate"
-    )
+    # parser.add_argument(
+    #     "--nthreads", type=int, default=1,
+    #     help="Number of threads to use, must be positive"
+    # )
 
-    parser.add_argument(
-        "--sponge", type=int, default=1, choices=[0, 1, 2],
-        help="Sponge function to use "\
-        "(0 is Blake2b, 1 is BlaMka, 2 is half-round BlaMka)"
-    )
+    # parser.add_argument(
+    #     "--nrounds", type=int, default=1, choices=list(range(1, 13)),
+    #     help="Number of rounds performed by reduced sponge function"
+    # )
 
-    parser.add_argument(
-        "--bench", type=bool, default=False,
-        help="Executable with built-in benchmarking"
-    )
+    # parser.add_argument(
+    #     "--nblocks", type=int, default=12, choices=[8, 10, 12],
+    #     help="Number of sponge blocks, bitrate"
+    # )
 
-    args = parser.parse_args()
+    # parser.add_argument(
+    #     "--sponge", type=int, default=1, choices=[0, 1, 2],
+    #     help="Sponge function to use "\
+    #     "(0 is Blake2b, 1 is BlaMka, 2 is half-round BlaMka)"
+    # )
 
-    if args.nthreads <= 0:
-        raise RuntimeError(
-            "--nthreads must be positive, was {}".format(args.nthreads)
-        )
+    # parser.add_argument(
+    #     "--bench", type=bool, default=False,
+    #     help="Executable with built-in benchmarking"
+    # )
 
-    make_lyra2(
-        option=args.option,
-        nCols=args.mcost,
-        nThreads=args.nthreads,
-        nRoundsSponge=args.nrounds,
-        bSponge=args.nblocks,
-        sponge=args.sponge
-    )
+    # args = parser.parse_args()
+
+    # if args.nthreads <= 0:
+    #     raise RuntimeError(
+    #         "--nthreads must be positive, was {}".format(args.nthreads)
+    #     )
+
+    # make_lyra2(
+    #     option=args.option,
+    #     nCols=args.mcost,
+    #     nThreads=args.nthreads,
+    #     nRoundsSponge=args.nrounds,
+    #     bSponge=args.nblocks,
+    #     sponge=args.sponge
+    # )
