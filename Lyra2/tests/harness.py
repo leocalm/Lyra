1c7811ad8e30dcf539ec290c702b8e28a73327da
Alexander Lisianoi <all3fox@gmail.com>
2017-05-27 15:29:45 +0200

    Setup logging

--- Lyra2/tests/harness.py
+++ Lyra2/tests/harness.py
@@ -4,7 +4,11 @@ import os
 import sys
 import yaml
 import subprocess
+import logging
+import logging.config
 import itertools
+
+logger = logging.getLogger(__name__)
 
 from pathlib import Path
 
@@ -247,18 +251,161 @@ def build_lyra2(params):
         parameters += ' -DBLOCK_LEN_INT64=' + str(blocks)
         parameters += ' -DBENCH=' + str(bench)
 
-        print('Trying to call make with these:')
-        print(makefile_path, makefile_path.parent)
+        logger.debug('Will be calling make with these these:')
+        logger.debug('--makefile ' + str(makefile_path))
+        logger.debug('--directory ' + str(makefile_path.parent))
+
+        executable = build_path.joinpath(name)
+
+        if executable.exists():
+            logger.info('Executable {} exists, will skip'.format(name))
+
+            continue
 
         process = subprocess.run([
             'make', option,
             'BINDIR=' + str(build_path),
-            'BIN=' + str(build_path.joinpath(name)),
+            'BIN=' + str(executable),
             parameters,
             'CFLAGS=' + CFLAGS,
             '--makefile', str(makefile_path),
             '--directory', str(makefile_path.parent),
-        ])
+        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        if process.returncode != 0:
+            logger.warning('Executable {} failed to build'.format(name))
+
+            logger.warning(process.stdout)
+            logger.warning(process.stderr)
+        else:
+            logger.info('Executable {} OK'.format(name))
+
+def valid_lyra2_output(out):
+    out = [
+        o.lstrip() for o in out.decode('utf-8').split('\n')
+        if o and o != '-' * 138
+    ]
+
+    valid = True
+
+    if len(out) != 16:
+        logger.warning('Output is {} instead of 16 items'.format(len(out)))
+
+        valid = False
+    elif out[0] != 'Inputs: ':
+        logger.warning('out[0] has changed')
+
+        valid = False
+    elif not out[1].startswith('Password: '):
+        logger.warning('out[1] has changed')
+
+        valid = False
+    elif not out[2].startswith('Password Length: '):
+        logger.warning('out[2] has changed')
+
+        valid = False
+    elif not out[3].startswith('Salt: '):
+        logger.warning('out[3] has changed')
+
+        valid = False
+    elif not out[4].startswith('Salt Length: '):
+        logger.warning('out[4] has changed')
+
+        valid = False
+    elif not out[5].startswith('Output Length: '):
+        logger.warning('out[5] has changed')
+
+        valid = False
+    elif out[6] != 'Parameters: ':
+        logger.warning('out[6] has changed')
+
+        valid = False
+    elif not out[7].startswith('T: '):
+        logger.warning('out[7] has changed')
+
+        valid = False
+    elif not out[8].startswith('R: '):
+        logger.warning('out[8] has changed')
+
+        valid = False
+    elif not out[9].startswith('C: '):
+        logger.warning('out[9] has changed')
+
+        valid = False
+    elif not out[10].startswith('Parallelism: '):
+        logger.warning('out[10] has changed')
+
+        valid = False
+    elif not out[11].startswith('Sponge: '):
+        logger.warning('out[11] has chagned')
+
+        valid = False
+    elif not out[12].startswith('Sponge Blocks (bitrate): '):
+        logger.warning('out[12] has changed')
+
+        valid = False
+    elif not out[13].startswith('Memory: '):
+        logger.warning('out[13] has changed')
+
+        valid = False
+    elif out[14] != 'Output: ':
+        logger.warning('out[14] has changed')
+
+        valid = False
+    elif not out[15].startswith('K: '):
+        logger.warning('out[15] has changed')
+
+        valid = False
+
+    if not valid:
+        logger.warning(out)
+
+        return None
+
+    pwd = out[1].split(': ')[1]
+    pwdlen = out[2].split(': ')[1]
+
+    if len(pwd) != int(pwdlen):
+        logger.warning('len({}) != {}'.format(pwd, pwdlen))
+
+        return None
+
+    salt = out[3].split(': ')[1]
+    saltlen = int(out[4].split(': ')[1])
+
+    if len(salt) != saltlen:
+        logger.warning('len({}) != {}'.format(salt, saltlen))
+
+    klen = int(out[5].split(': ')[1])
+    tcost = int(out[7].split(': ')[1])
+    mcost = int(out[8].split(': ')[1])
+
+    columns = int(out[9].split(': ')[1])
+    threads = int(out[10].split(': ')[1])
+
+    sponge = out[11].split(': ')[1]
+    blocks = int(out[12].split(': ')[1].split(' = ')[0])
+
+    data = [
+        digit if len(digit) == 2 else '0' + digit
+        for digit in out[15].split(': ')[1].split('|') if digit
+    ]
+
+    if len(data) != klen:
+        logger.warning('len({}) != {}'.format(data, klen))
+
+    return {
+        'pwd': pwd,
+        'salt': salt,
+        'klen': klen,
+        'tcost': tcost,
+        'mcost': mcost,
+        'columns': columns,
+        'threads': threads,
+        'sponge': sponge,
+        'blocks': blocks,
+        'hash': data,
+    }
 
 def compute_data(params):
     valid_lyra2_hashes_or_exit(params)
@@ -282,23 +429,66 @@ def compute_data(params):
 
         [sponge, sponge_idx] = compose_sponge_name(sponge)
 
-        name = build_path.joinpath(compose_lyra2_name(
+        name = compose_lyra2_name(
             option, threads, columns, sponge, rounds, blocks
-        ))
+        )
 
-        for data in unlist_values(params['data']):
-            pwd = str(data['pwd'])
-            salt = str(data['salt'])
-            klen = str(data['klen'])
-            tcost = str(data['tcost'])
-            mcost = str(data['mcost'])
+        executable = build_path.joinpath(name)
+
+        if not executable.exists():
+            logger.warning('Executable {} not there'.format(name))
+            logger.warning('Did you forget to run build?')
+
+            continue
+
+        data_path.joinpath(name).mkdir(parents=True, exist_ok=True)
+
+        data_file = data_path.joinpath(name, 'data.yml')
+        if data_file.exists():
+            logger.info(
+                'File {} exists, skipping'.format(Path(name, 'data.yml'))
+            )
+
+            continue
+
+        data = {}
+
+        for i, entry in enumerate(unlist_values(params['data'])):
+            pwd = str(entry['pwd'])
+            salt = str(entry['salt'])
+            klen = str(entry['klen'])
+            tcost = str(entry['tcost'])
+            mcost = str(entry['mcost'])
 
             process = subprocess.run(
-                [name, pwd, salt, klen, tcost, mcost],
+                [executable, pwd, salt, klen, tcost, mcost],
                 stdout=subprocess.PIPE, stderr=subprocess.PIPE,
             )
 
-            print(process)
+            logger.info(
+                'Compute pwd: {} salt: {} klen: {} tcost: {} mcost: {}'.format(
+                pwd, salt, klen, tcost, mcost
+            ))
+
+            if process.returncode != 0:
+                logger.warning('Failed to compute hash')
+
+                logger.warning(process.stdout)
+                logger.warning(process.stderr)
+
+                continue
+
+            out = valid_lyra2_output(process.stdout)
+
+            data['entry' + str(i)] = out
+
+            logger.info('Result: ' + ' '.join(out['hash']))
+
+            if process.stderr:
+                logger.warning(process.stderr)
+
+        with open(data_file, 'w') as dst:
+            yaml.dump(data, dst)
 
 if __name__ == '__main__':
     import yaml
@@ -309,7 +499,7 @@ if __name__ == '__main__':
     parser = ArgumentParser(description='Friendly lyra2 frontend')
 
     parser.add_argument(
-        '--yaml', default=path.joinpath('lyra2.yml'),
+        '--yaml', default=path.joinpath('harness.yml'),
         help='set location of settings file'
     )
 
@@ -329,9 +519,13 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     with open(args.yaml, 'r') as config:
+        params = yaml.load(config)
+
+        logging.config.dictConfig(params['logging'])
+
         if args.cmd == 'build':
-            build_lyra2(yaml.load(config))
+            build_lyra2(params)
         elif args.cmd == 'compute':
-            compute_data(yaml.load(config))
+            compute_data(params)
         else:
             sys.exit('Unknown args.cmd')
