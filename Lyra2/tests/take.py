2afa41b62c064fd5abb84cf253cf7a519cc155e1
Alexander Lisianoi <all3fox@gmail.com>
2017-08-20 15:59:51 +0200

    Make take.py select N random tests

--- Lyra2/tests/take.py
+++ Lyra2/tests/take.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 
 if __name__ == "__main__":
+    import sys
     import yaml
+    import random
+
     from pathlib import Path
     from argparse import ArgumentParser
 
@@ -9,6 +12,9 @@ if __name__ == "__main__":
 
     parser.add_argument(
         "N", default=100, type=int, help="the number of hashes to take"
+    )
+    parser.add_argument(
+        '--seed', default=42, type=int, help='initialize the random generator'
     )
     parser.add_argument(
         "src", help="path to data.yml to take hashes from"
@@ -21,13 +27,20 @@ if __name__ == "__main__":
     src_fname = Path(args.src).resolve()
     dst_fname = Path(path, "data-" + str(args.N) + ".yml")
 
-    entries = []
+    N, entries = args.N, []
     with open(src_fname, "r") as src:
-        for i, entry in enumerate(yaml.load_all(src)):
-            if i == args.N:
-                break
+        data = [entry for entry in yaml.load_all(src)]
 
-            entries.append(entry)
+        if N > len(data):
+            print("You've asked for " + N + " hash(es)")
+            print("The provided data file only has " + len(data))
 
-    yaml.dump_all(entries, open(dst_fname, "w"))
-    
+            sys.exit("Provided data file does not have enough data")
+
+        random.seed(args.seed)
+        entries = random.sample(data, N)
+
+    if entries:
+        yaml.dump_all(entries, open(dst_fname, "w"))
+    else:
+        sys.exit("something went wrong")
